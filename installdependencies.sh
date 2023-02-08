d402783cf351fa63376aa9ad5ff63b900e301fea
yukutonet <amaji.yukuto@gmail.com>
2023-02-09 00:15:51 +0900

    環境構築シェルを微修正

--- installdependencies.sh
+++ installdependencies.sh
@@ -6,7 +6,9 @@
 cd ./lib/
 
 # ndc-cpp11-libs のビルドを行う
+echo "[INFO] BUILD NDC-CPP11-LIBS BEGINS..."
 rm -rf ndc-cpp11-libs
 git clone https://github.com/nodachisoft-com/ndc-cpp11-libs.git
 cd ndc-cpp11-libs
 sh build.sh
+echo "[INFO] BUILD NDC-CPP11-LIBS COMPLETED!"
